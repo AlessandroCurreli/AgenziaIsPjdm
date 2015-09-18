57b9a0975dd463b91e09aacfcc37d2bc031bdb44
adrian <adrian.drob88@gmail.com>
2015-09-18 17:27:45 +0200

    modifica al layout della ricerca e implementata tutta guidata con provincie e citta elencate tutte come risorsa al interno del file strings.xml, IMPORTANTE!!! l'applicazione ora fa accesso al server locale

--- app/src/main/java/com/curdrome/agenziaispjdm/research/SearchFragment.java
+++ app/src/main/java/com/curdrome/agenziaispjdm/research/SearchFragment.java
@@ -26,7 +26,9 @@ import org.json.JSONObject;
  */
 public class SearchFragment extends android.support.v4.app.Fragment {
 
+    // json che contiene tutti i campi scelti
     JSONObject selection = new JSONObject();
+    // collegamento alla activity
     ResearchActivity activity;
 
     public SearchFragment() {
@@ -51,22 +53,8 @@ public class SearchFragment extends android.support.v4.app.Fragment {
     public void onViewCreated(final View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        final Button btn = (Button) view.findViewById(R.id.idButtonSearch);
+        // redio group per la gestione della scelta della fascia di prezzo
         RadioGroup prezziRadioGroup = (RadioGroup) view.findViewById(R.id.radioGroupPrezzi);
-        RadioGroup mqRadioGroup = (RadioGroup) view.findViewById(R.id.radioGroupMq);
-        EditText bagniEditText = (EditText) view.findViewById(R.id.idNbagni);
-        EditText camereEditText = (EditText) view.findViewById(R.id.idNcamere);
-
-        final Spinner spinner1 = (Spinner) view.findViewById(R.id.propertiesTypes);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
-                R.array.types_array, android.R.layout.simple_spinner_dropdown_item);
-        // Specify the layout to use when the list of choices appears
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner1.setAdapter(adapter1);
-
-        final Spinner spinner2 = (Spinner) view.findViewById(R.id.propertiesSecondTypes);
 
         prezziRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -104,6 +92,10 @@ public class SearchFragment extends android.support.v4.app.Fragment {
                 }
             }
         });
+
+        //radio group per la scelta della fascia dei metri quadri
+
+        RadioGroup mqRadioGroup = (RadioGroup) view.findViewById(R.id.radioGroupMq);
 
         mqRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -150,6 +142,10 @@ public class SearchFragment extends android.support.v4.app.Fragment {
             }
         });
 
+        // gestione della edit text per il numero di bagni
+
+        EditText bagniEditText = (EditText) view.findViewById(R.id.idNbagni);
+
         bagniEditText.addTextChangedListener(new TextWatcher() {
 
             @Override
@@ -170,6 +166,10 @@ public class SearchFragment extends android.support.v4.app.Fragment {
                 }
             }
         });
+
+        // gestione della edit text per la gestione del numero delle camere
+
+        EditText camereEditText = (EditText) view.findViewById(R.id.idNcamere);
 
         camereEditText.addTextChangedListener(new TextWatcher() {
 
@@ -192,7 +192,22 @@ public class SearchFragment extends android.support.v4.app.Fragment {
             }
         });
 
-        spinner1.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+        // spinner per la scelta del tipo
+
+        final Spinner spinnerTypes = (Spinner) view.findViewById(R.id.propertiesTypes);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                R.array.types_array, android.R.layout.simple_spinner_dropdown_item);
+        // Specify the layout to use when the list of choices appears
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerTypes.setAdapter(adapter1);
+
+        // spinner condizionato per la scelta del sottotipo
+
+        final Spinner spinnerSubtype = (Spinner) view.findViewById(R.id.propertiesSecondTypes);
+
+        spinnerTypes.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 try {
@@ -200,22 +215,22 @@ public class SearchFragment extends android.support.v4.app.Fragment {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                if (spinner1.getSelectedItem().equals("commerciale")) {
+                if (spinnerTypes.getSelectedItem().equals("commerciale")) {
                     // Create an ArrayAdapter using the string array and a default spinner layout
                     ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
                             R.array.commerciale_array, android.R.layout.simple_spinner_dropdown_item);
                     // Specify the layout to use when the list of choices appears
                     adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     // Apply the adapter to the spinner
-                    spinner2.setAdapter(adapter2);
-                } else if (spinner1.getSelectedItem().equals("residenziale")) {
+                    spinnerSubtype.setAdapter(adapter2);
+                } else if (spinnerTypes.getSelectedItem().equals("residenziale")) {
                     // Create an ArrayAdapter using the string array and a default spinner layout
                     ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
                             R.array.residenziale_array, android.R.layout.simple_spinner_dropdown_item);
                     // Specify the layout to use when the list of choices appears
                     adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     // Apply the adapter to the spinner
-                    spinner2.setAdapter(adapter2);
+                    spinnerSubtype.setAdapter(adapter2);
                 } else {
                     // Create an ArrayAdapter using the string array and a default spinner layout
                     ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
@@ -223,7 +238,7 @@ public class SearchFragment extends android.support.v4.app.Fragment {
                     // Specify the layout to use when the list of choices appears
                     adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     // Apply the adapter to the spinner
-                    spinner2.setAdapter(adapter2);
+                    spinnerSubtype.setAdapter(adapter2);
                 }
             }
 
@@ -232,7 +247,7 @@ public class SearchFragment extends android.support.v4.app.Fragment {
             }
         });
 
-        spinner2.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+        spinnerSubtype.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -249,8 +264,55 @@ public class SearchFragment extends android.support.v4.app.Fragment {
             }
         });
 
+        // spinner per la scelta della provincia
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        final Spinner spinnerProvincia = (Spinner) view.findViewById(R.id.provincia);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        final ArrayAdapter<CharSequence> adapterProvincia = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                R.array.Province, android.R.layout.simple_spinner_dropdown_item);
+        // Specify the layout to use when the list of choices appears
+        adapterProvincia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerProvincia.setAdapter(adapterProvincia);
+
+        // spinner per la scelta della citta
+
+        final Spinner spinnerCitta = (Spinner) view.findViewById(R.id.citta);
+
+        spinnerProvincia.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                try {
+                    selection.put("province", parent.getItemAtPosition(position).toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Object scelta = parent.getItemAtPosition(position);
+
+                int[] arrayCitta = {R.array.AG, R.array.AL, R.array.AN, R.array.BA, R.array.BG, R.array.BO, R.array.CT, R.array.FI, R.array.GE, R.array.MI, R.array.NA, R.array.PA, R.array.PG, R.array.RC, R.array.TO, R.array.TN, R.array.VE};
+
+                ArrayAdapter<CharSequence> adapterCitta = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                        arrayCitta[position], android.R.layout.simple_spinner_dropdown_item);
+                // Specify the layout to use when the list of choices appears
+                adapterCitta.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                // Apply the adapter to the spinner
+                spinnerCitta.setAdapter(adapterCitta);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        // bottone di conferma che invia il json creato alla asyncTask
+
+        final Button btn = (Button) view.findViewById(R.id.idButtonSearch);
+
+        btn.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
             public void onClick(View view) {
                 Log.d("AGENZIAISPJDM", selection.toString());
