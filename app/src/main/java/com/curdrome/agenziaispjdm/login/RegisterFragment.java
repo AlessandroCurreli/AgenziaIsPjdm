5a9efea6836a5a878fc1d4b14ea4f1972007425e
Alessandro Curreli <evanescent89@gmail.com>
2015-09-23 15:30:43 +0200

    aggiunta di "_" alla possibile password

--- app/src/main/java/com/curdrome/agenziaispjdm/login/RegisterFragment.java
+++ app/src/main/java/com/curdrome/agenziaispjdm/login/RegisterFragment.java
@@ -54,7 +54,7 @@ public class RegisterFragment extends android.support.v4.app.Fragment {
 
     // validating password with retype password
     static protected boolean isValidPassword(String pass) {
-        String PWD_PATTERN = "^(?=[^\\d_].*?\\d)\\w(\\w|[!@#$%]){7,20}";
+        String PWD_PATTERN = "^(?=[^\\d_].*?\\d)\\w(\\w|[_!@#$%]){7,20}";
         Pattern pattern = Pattern.compile(PWD_PATTERN);
         Matcher matcher = pattern.matcher(pass);
         return matcher.matches();
