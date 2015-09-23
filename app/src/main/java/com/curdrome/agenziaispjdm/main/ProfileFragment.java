c072bae5a92f5d96a21491259d35a83a13f8a32f
adrian <adrian.drob88@gmail.com>
2015-09-23 15:22:25 +0200

    -aggiunta la gestione del profilo in main activity -aggiunto nel fragment del profilo il prelevamento dei dati del utente -creato il layout con i campi modificabili del utente -in strings aggiunto il titolo del profilo

--- app/src/main/java/com/curdrome/agenziaispjdm/main/ProfileFragment.java
+++ app/src/main/java/com/curdrome/agenziaispjdm/main/ProfileFragment.java
@@ -6,19 +6,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.curdrome.agenziaispjdm.R;
+import com.curdrome.agenziaispjdm.model.User;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends android.support.v4.app.Fragment {
 
+    MainActivity activity;
+
+    User user;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = (MainActivity) getActivity();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,4 +39,25 @@ public class ProfileFragment extends Fragment {
     }
 
 
+    @Override
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        activity.getActionBar().setTitle(R.string.title_profile_fragment);
+
+        user = activity.getUser();
+
+        EditText mail = (EditText) view.findViewById(R.id.email_text);
+        EditText name = (EditText) view.findViewById(R.id.name_text);
+        EditText surname = (EditText) view.findViewById(R.id.surname_text);
+        EditText telefono = (EditText) view.findViewById(R.id.phone_text);
+        EditText password = (EditText) view.findViewById(R.id.password_text);
+
+        mail.setText(user.getLogin(), TextView.BufferType.SPANNABLE);
+        name.setText(user.getFirstname(), TextView.BufferType.SPANNABLE);
+        surname.setText(user.getLastname(), TextView.BufferType.SPANNABLE);
+        telefono.setText("" + user.getPhone(), TextView.BufferType.SPANNABLE);
+        password.setText(user.getPassword(), TextView.BufferType.SPANNABLE);
+
+    }
 }
